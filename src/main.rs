d30c8517414e7e241b97a1180fe659bf9e5020cd
EasChen70 <eason_chen@uri.edu>
2024-01-30 23:11:23 -0500

    Worked on splitting string into fingerprint/name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use std::io;
+use std::io::{self, BufRead};
+use std::collections::HashMap;
 
 fn main() {
-    let mut input = String::new();
+    //Takes multilined input, and loops lines
+    let input = io::stdin();
+    for line in input.lock().lines(){
+        if let Ok(line) = line{
+            println!("{}", line);
 
-    let input = input.trim().to_string();
+            //Splits fingerprint and name
+            let mut split_input = line.splitn(2, ' ');
+            if let (Some(fingerprint), Some(name)) = (split_input.next(), split_input.next()){
+                println!("Fingerprint: {}", fingerprint);
+                println!("Name: {}", name);
+            }
+        }
+    }
 
+
+    
+    let mut fingerprint_map: HashMap<String, Vec<String>> = HashMap::new();
 }
