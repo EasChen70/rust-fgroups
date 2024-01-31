fe2ce422896e755aaad1a09cb7767dcb0e6ad6e6
EasChen70 <eason_chen@uri.edu>
2024-01-31 01:50:39 -0500

    finished code for part B, reads from file though

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 use std::io::{self, BufRead};
 use std::collections::HashMap;
+use std::fs::File;
 
 fn main() {
-    //Takes multilined input, and loops lines
-    let input = io::stdin();
-    for line in input.lock().lines(){
+    //Open, and reads file, assigning to input
+    let file = File::open("input.txt").expect("Failed to open input.txt");
+    let input = io::BufReader::new(file);
+    //Initialize hashmap
+    let mut fingerprint_map: HashMap<String, Vec<String>> = HashMap::new();
+    //loop over lines of input to process
+    for line in input.lines(){
         if let Ok(line) = line{
-            println!("{}", line);
-
+        
             //Splits fingerprint and name
             let mut split_input = line.splitn(2, ' ');
+            //Option, assigns fingerprint(key) and name(value), to substrings of split
             if let (Some(fingerprint), Some(name)) = (split_input.next(), split_input.next()){
-                println!("Fingerprint: {}", fingerprint);
-                println!("Name: {}", name);
+
+                //insert key-value pairs into hashmap
+                fingerprint_map.entry(fingerprint.to_string())
+                .or_insert_with(Vec::new).push(name.to_string());
             }
         }
     }
-
-
     
-    let mut fingerprint_map: HashMap<String, Vec<String>> = HashMap::new();
+    //printing function, enumerate allows us to track of index as well
+    for (index, (_key, values)) in fingerprint_map.iter().enumerate(){
+        if values.len() > 1{
+            for(_index, value) in values.iter().enumerate(){
+                println!("{}", value);            
+            }
+            //checks if any more groups left, if its not last one, print a blank space
+            if index < fingerprint_map.len() - 1{
+                println!();
+            }    
+        }
+    }
 }
